04e55929dd14200745974478bc5809a96fcf93c9
andy <andydavies2020@gmail.com>
2019-02-24 13:48:20 +0200

    demoService: blank app

---
+++ demo_service/src/DemoService/Program.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.Extensions.Configuration;
+
+namespace DemoService
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var config = new Configuration();
+            new ConfigurationBuilder()
+                .AddEnvironmentVariables()
+                .Build()
+                .Bind(config);
+        }
+    }
+
+    public class Configuration
+    {
+        public string VaultRoleID { get; set; }
+        public string VaultSecretID { get; set; }
+    }
+}
