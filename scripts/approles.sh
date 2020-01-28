0fad0066d869420d4c5ecfdfc48b377d0b9b0f7e
andy <andy.davies@reaktor.com>
2020-01-28 18:19:29 +0200

    scripts; readd the printlines, it's prettier

--- scripts/approles.sh
+++ scripts/approles.sh
@@ -1,11 +1,21 @@
-#!/bin/bash -eux
+#!/bin/bash -eu
 
 # enable AppRole backend
+echo "
+vault auth enable approle
+"
+
 vault auth enable approle
 
 read -n 1 -s -r
 
 # ci could read the policies and update vault (after PR, of course...)
+echo '
+vault write auth/approle/role/demo_service
+    token_ttl=20m
+    token_max_ttl=1h
+    policies="default, postgres_connector"
+'
 vault write auth/approle/role/demo_service \
     token_ttl=20m \
     token_max_ttl=1h \
@@ -14,9 +24,15 @@ vault write auth/approle/role/demo_service \
 read -n 1 -s -r
 
 # read the generated role_id:
+echo '
+vault read auth/approle/role/demo_service/role-id
+'
 vault read auth/approle/role/demo_service/role-id
 
 read -n 1 -s -r
 
 # create a secret id for the instance of the application
+echo '
+vault write -f auth/approle/role/demo_service/secret-id
+'
 vault write -f auth/approle/role/demo_service/secret-id
--- scripts/postgres.sh
+++ scripts/postgres.sh
@@ -1,12 +1,21 @@
-#!/bin/bash -eux
+#!/bin/bash -eu
+
+# export VAULT_ADDR="http://$(cat .machine/ip):8200"
+# export VAULT_TOKEN="$(cat .machine/vault_token)"
 
 # enable database secrets engine
 echo "vault secrets enable database"
-vault secrets enable database || echo "database secrets engine already enabled"
+vault secrets enable database 2>/dev/null || echo "database secrets engine already enabled"
 
 read -n 1 -s -r
 
 # create a policy allowing reads from the database secret engine
+echo "
+echo 'path \"database/creds/*\" {
+  capabilities = [\"read\"]
+}' | vault policy write postgres_connector -
+"
+
 echo 'path "database/creds/*" {
   capabilities = ["read"]
 }' | vault policy write postgres_connector -
@@ -14,6 +23,15 @@ echo 'path "database/creds/*" {
 read -n 1 -s -r
 
 # configure how to communicate with postgres
+echo '
+vault write database/config/postgres
+  plugin_name="postgresql-database-plugin"
+  allowed_roles="*"
+  connection_url="postgresql://{{username}}:{{password}}@postgres.service.consul/postgres?sslmode=disable"
+  username="postgres"
+  password="postgres"
+'
+
 vault write database/config/postgres \
   plugin_name="postgresql-database-plugin" \
   allowed_roles="*" \
@@ -24,6 +42,16 @@ vault write database/config/postgres \
 read -n 1 -s -r
 
 # create the "reader" dbrole: attached to postgres, and can do CRUD operations on tables
+echo '
+vault write database/roles/reader
+  db_name="postgres"
+  creation_statements="
+    CREATE ROLE \"{{name}}\" WITH LOGIN PASSWORD "'"{{password}}"'" VALID UNTIL "'"{{expiration}}"'";
+    GRANT SELECT ON ALL TABLES IN SCHEMA public TO \"{{name}}\";"
+  default_ttl="10m"
+  max_ttl="1h"
+'
+
 vault write database/roles/reader \
   db_name="postgres" \
   creation_statements=" \
@@ -35,6 +63,16 @@ vault write database/roles/reader \
 read -n 1 -s -r
 
 # create the "writer" dbrole: attached to postgres, and can do CRUD operations on tables
+echo '
+vault write database/roles/writer
+  db_name="postgres"
+  creation_statements="
+    CREATE ROLE \"{{name}}\" WITH LOGIN PASSWORD "'"{{password}}"'" VALID UNTIL "'"{{expiration}}"'";
+    GRANT SELECT, INSERT, UPDATE, DELETE ON ALL TABLES IN SCHEMA public TO \"{{name}}\";"
+  default_ttl="10m"
+  max_ttl="1h"
+'
+
 vault write database/roles/writer \
   db_name="postgres" \
   creation_statements=" \
@@ -42,5 +80,3 @@ vault write database/roles/writer \
     GRANT SELECT, INSERT, UPDATE, DELETE ON ALL TABLES IN SCHEMA public TO \"{{name}}\";" \
   default_ttl="10m" \
   max_ttl="1h"
-
-read -n 1 -s -r
